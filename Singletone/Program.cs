c55dc2dab7fa9ffa2e2eda07d715a28adda975ec
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-01-24 06:32:24 +0300

    Add project files.

---
+++ Singletone/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singletone instance1 = Singletone.Instance();
+            Singletone instance2 = Singletone.Instance();
+            Console.WriteLine(ReferenceEquals(instance1, instance2));
+
+            instance1.SingletonOperation();
+            string singletonData = instance1.GetSingletoneData();
+
+            Console.WriteLine(singletonData);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singletone/Singletone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singletone
+{
+    internal class Singletone
+    {
+        static Singletone uniqueInstance;
+        string singletoneData = string.Empty;
+        protected Singletone()
+        { }
+        public static Singletone Instance()
+        {
+            if (uniqueInstance == null)
+                uniqueInstance = new Singletone();
+            return uniqueInstance;
+        }
+
+        internal void SingletonOperation()
+        {
+            singletoneData = "SingletoneData";
+        }
+
+        internal string GetSingletoneData()
+        {
+            return singletoneData;
+        }
+    }
+}
